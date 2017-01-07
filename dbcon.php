51c649c242361b863cf1b2dcd4a7ae92478f6281
nehaevm14 <neha@expertvillagemedia.com>
2017-01-07 17:55:47 +0530

    Delete dbcon.php

--- dbcon.php
+++
@@ -1,7 +0,0 @@
-<?php
-$conn=mysql_connect("localhost","expertvi_app","Ll@hpTa[s+mS") or die("sorry! No server not found");
-$select_db=mysql_select_db("expertvi_galleryapp", $conn) or die("database not selected...!!!");
-// if($select_db==1)
-// {
-// }
-?>
